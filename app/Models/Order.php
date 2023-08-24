a59ba0d5283a8b8cb88b8ca47a7613e0c70d2a27
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-08-24 17:02:46 +0700

    create model Order and fillable

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['customer_id', 'number', 'total_price', 'status', 'shipping_price', 'notes'];
+
+    public function customers(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function items(): HasMany
+
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
