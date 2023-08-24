9dd817a14216319d993095eb5970e26c307e9c0f
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-08-24 17:02:11 +0700

    create model brand an fillable

---
+++ app/Models/Brand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Brand extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'slug', 'url', 'primary_hex', 'is_visible', 'description'];
+
+    public function brands(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
