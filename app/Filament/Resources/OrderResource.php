cbd38fc45f227b9d8d8873e8c45ed22d382a6fbb
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-08-29 11:00:01 +0700

    update colomn and form

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\OrderstatusEnum;
 use Filament\Forms;
 use Filament\Tables;
 use App\Models\Order;
@@ -15,6 +16,15 @@ use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\OrderResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
+use Filament\Tables\Columns\Summarizers\Sum;
+use Filament\Tables\Columns\TextColumn;
 
 class OrderResource extends Resource
 {
@@ -27,7 +37,27 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Wizard::make([
+                    Step::make('Order Details')->schema([
+                        TextInput::make('number')->default('OR-' . random_int(100000, 999999))->disabled()->dehydrated()->required(),
+                        Select::make('customer_id')->relationship('customers', 'name')->searchable()->required(),
+                        Select::make('type')->options([
+                            'pending' => OrderstatusEnum::PENDING->value,
+                            'processing' => OrderstatusEnum::PROCESSING->value,
+                            'completed' => OrderstatusEnum::PENDING->value,
+                            'declined' => OrderstatusEnum::DECLINED->value,
+                        ])->columnSpanFull()->required(),
+                        MarkdownEditor::make('notes')->columnSpanFull(),
+                    ])->columns(2),
+
+                    Step::make('Order Items')->schema([
+                        Repeater::make('items')->relationship()->schema([
+                            Select::make('product_id')->label('Product')->options(Product::query()->pluck('name', 'id')),
+                            TextInput::make('quantity')->numeric()->default(1)->required(),
+                            TextInput::make('unit_price')->label('Unit Price')->disabled()->dehydrated()->numeric()->required()
+                        ])->columns(3)
+                    ]),
+                ])->columnSpanFull(),
             ]);
     }
 
@@ -35,7 +65,13 @@ class OrderResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('number')->searchable()->sortable(),
+                TextColumn::make('customer.name')->searchable()->sortable()->toggleable(),
+                TextColumn::make('status')->searchable()->sortable(),
+                TextColumn::make('total_price')->searchable()->sortable()->summarize([
+                    Sum::make()->money()
+                ]),
+                TextColumn::make('created_at')->label('Order Date')->date()
             ])
             ->filters([
                 //
