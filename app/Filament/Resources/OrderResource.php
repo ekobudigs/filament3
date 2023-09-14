70fc23dd0be030b9e8b23680a919db146082ad99
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-09-14 16:28:47 +0700

    update form order resource

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -2,29 +2,31 @@
 
 namespace App\Filament\Resources;
 
-use App\Enums\OrderstatusEnum;
 use Filament\Forms;
 use Filament\Tables;
 use App\Models\Order;
+use App\Models\Product;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
+use App\Enums\OrderstatusEnum;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Repeater;
 use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\DeleteAction;
 use Illuminate\Database\Eloquent\Builder;
-use App\Filament\Resources\OrderResource\Pages;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use App\Filament\Resources\OrderResource\RelationManagers;
-use App\Models\Product;
-use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Wizard\Step;
 use Filament\Tables\Columns\Summarizers\Sum;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\OrderResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
+use App\Filament\Resources\OrderResource\RelationManagers;
 
 class OrderResource extends Resource
 {
@@ -33,6 +35,14 @@ class OrderResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?int $navigationSort = 3;
     protected static ?string $navigationGroup = 'Shop';
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::where('status', '=', 'processing')->count();
+    }
+    public static function getNavigationBadgeColor(): ?string
+    {
+        return static::getModel()::where('status', '=', 'processing')->count() > 10 ? 'warning' : 'primary';
+    }
     public static function form(Form $form): Form
     {
         return $form
@@ -40,22 +50,28 @@ class OrderResource extends Resource
                 Wizard::make([
                     Step::make('Order Details')->schema([
                         TextInput::make('number')->default('OR-' . random_int(100000, 999999))->disabled()->dehydrated()->required(),
-                        Select::make('customer_id')->relationship('customers', 'name')->searchable()->required(),
+                        Select::make('customer_id')->relationship('customer', 'name')->searchable()->required(),
+                        TextInput::make('shipping_price')->label('Shipping Costs')->dehydrated()->numeric()->required(),
                         Select::make('type')->options([
                             'pending' => OrderstatusEnum::PENDING->value,
                             'processing' => OrderstatusEnum::PROCESSING->value,
-                            'completed' => OrderstatusEnum::PENDING->value,
+                            'completed' => OrderstatusEnum::COMPLETED->value,
                             'declined' => OrderstatusEnum::DECLINED->value,
-                        ])->columnSpanFull()->required(),
+                        ])->required(),
                         MarkdownEditor::make('notes')->columnSpanFull(),
                     ])->columns(2),
 
                     Step::make('Order Items')->schema([
                         Repeater::make('items')->relationship()->schema([
-                            Select::make('product_id')->label('Product')->options(Product::query()->pluck('name', 'id')),
-                            TextInput::make('quantity')->numeric()->default(1)->required(),
-                            TextInput::make('unit_price')->label('Unit Price')->disabled()->dehydrated()->numeric()->required()
-                        ])->columns(3)
+                            Select::make('product_id')->label('Product')->options(Product::query()->pluck('name', 'id'))->required()->reactive()->afterStateUpdated(fn($state, Forms\Set $set) => 
+                            $set('unit_price', Product::find($state)?->price ?? 0)),
+                            TextInput::make('quantity')->numeric()->default(1)->required()->live()->dehydrated(),
+                            TextInput::make('unit_price')->label('Unit Price')->disabled()->dehydrated()->numeric()->required(),
+
+                            Placeholder::make('total_price')->label('Total Price')->content(function ($get) {
+                                return $get('quantity') * $get('unit_price');
+                            })
+                        ])->columns(4)
                     ]),
                 ])->columnSpanFull(),
             ]);
@@ -68,9 +84,6 @@ class OrderResource extends Resource
                 TextColumn::make('number')->searchable()->sortable(),
                 TextColumn::make('customer.name')->searchable()->sortable()->toggleable(),
                 TextColumn::make('status')->searchable()->sortable(),
-                TextColumn::make('total_price')->searchable()->sortable()->summarize([
-                    Sum::make()->money()
-                ]),
                 TextColumn::make('created_at')->label('Order Date')->date()
             ])
             ->filters([
@@ -85,6 +98,7 @@ class OrderResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
+                    ExportBulkAction::make(),
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ])
