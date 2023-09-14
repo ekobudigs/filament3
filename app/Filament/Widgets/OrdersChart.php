c95d49beba881e929dbe59532d918319bb56bc0b
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-09-14 16:29:34 +0700

    create widget order chart

---
+++ app/Filament/Widgets/OrdersChart.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\OrderstatusEnum;
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
+
+class OrdersChart extends ChartWidget
+{
+    protected static ?int $sort = 3;
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {      $data = Order::select('status', DB::raw('count(*) as count'))->groupBy('status')->pluck('count', 'status')->toArray();
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Orders',
+                    'data' => array_values($data)
+                ]
+                ],
+                'labels' => OrderstatusEnum::cases()
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
