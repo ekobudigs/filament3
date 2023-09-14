6b31b8f3c38d965078bf8e32daa3a24b2783d1ae
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-09-14 16:30:33 +0700

    update name function

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -11,7 +11,7 @@ class Brand extends Model
     use HasFactory;
     protected $fillable = ['name', 'slug', 'url', 'primary_hex', 'is_visible', 'description'];
 
-    public function brands(): HasMany
+    public function products(): HasMany
     {
         return $this->hasMany(Product::class);
     }
